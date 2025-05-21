ac0923e5d0cbdc7cc6b054b8b547fbd7a18a2243
Marga <marga.llompart.75@gmail.com>
2025-05-21 18:34:33 +0200

    Cambiamos el frontend
    Me descargue mal el proyecto en react y no me funcionaban los estilos de Tilwind css. Este nuevo proyecto si funciona.
    Con esto, la pagina de inicio (Login) ya se ve correctamente

--- frontend/EquusTrackFrontend/src/App.jsx
+++ frontend/EquusTrackFrontend/src/App.jsx
@@ -1,30 +1,15 @@
-import "./App.css";
-import { useState } from "react";
-import Registro from "./components/Registro";
-import Login from "./components/Login";
-import PantallaPrincipal from "./components/PantallaPrincipal";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Registro from "./pages/Registro";
 
 function App() {
-  const [usuario, setUsuario] = useState(null);
-
   return (
-    
-    /* <>
-      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-        <Registro />
-      </div>
-    </> */
-
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      {!usuario ? (
-        <Login onLoginSuccess={(data) => setUsuario(data)} />
-      ) : (
-        <div>
-          <PantallaPrincipal usuario={usuario} />
-        </div>
-      )}
-    </div>
-
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/registro" element={<Registro />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/EquusTrackFrontend/src/pages/Registro.jsx
@@ -0,0 +1,10 @@
+export default function Registro() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-slate-100">
+      <div className="p-8 bg-white shadow rounded">
+        <h1 className="text-2xl font-bold mb-4">Registro</h1>
+        <p>Aquí irá el formulario de registro.</p>
+      </div>
+    </div>
+  );
+}
