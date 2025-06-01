970d293a1abd5d8199a392f43e14e5002e6fdd3b
Marga <marga.llompart.75@gmail.com>
2025-06-01 20:00:23 +0200

    Página detalles del entrenamiento funcioando correctamente con el backend

---
+++ backend/EquusTrackBackend/Models/EjercicioEntrenamiento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquusTrackBackend.Models
+{
+    public class EjercicioEntrenamiento
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string? Descripcion { get; set; }
+        public int DuracionSegundos { get; set; }
+        public int? Repeticiones { get; set; }
+        public string TipoBloque { get; set; } // "Calentamiento", "Principal", "VueltaCalma"
+        public string? ImagenURL { get; set; }
+    }
+}
